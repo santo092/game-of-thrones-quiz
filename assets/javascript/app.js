4519546e8ecda38253546d681ba3092656327a58
Brandon Harris <BrandonMGH@Yahoo.com>
2019-03-10 15:09:12 -0700

    Update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,26 @@
-$("#button1").click(function (event) {
+
+
+// <---INDEX PAGE JAVASCRIPT--->
+console.log("Hi")
+
+  $("#button1").click(function (event) {
    
     window.location.href = "questions.html";
 
     });
+
+
+// <---QUESTION PAGE JAVASCRIPT ---> 
+
+var houseTargTally = 0;
+var houseStarkTally = 0;
+var houseLannTally = 0; 
+var houseMartellTally = 0;
+var wildlingTally = 0; 
+
+$('input[name="radio"]').on('click', function() {
+    if ($(this).val() == '1') {
+        houseTargTally++;
+        console.log(houseTargTally);
+    }
+}); 
