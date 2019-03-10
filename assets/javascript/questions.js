f055b02e270ddfa96a26232e3cd87d70018c962b
Alexis Villalpando <alexisvillalpando@Tsmacbookpro.local>
2019-03-10 16:40:57 -0700

    added point system leading to different houses html

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,101 +1,152 @@
+
+ $("#button1").click(function (event) {
+   
+  window.location.href = "questions.html";
+
+ });
+
+
 var questions = [{
     "question" : " aaaaaaaaaa?",
-    "option1" : " Abacus", 
-    "option2" : " Aus", 
-    "option3" : " Abus", 
-    "option4" : " Abus", 
-    "option5" : " Abus", 
-    "option6" : " Abus", 
-    "answer": "2"
+    "option1" : " liberals- martells", 
+    "option2" : " libertarians-greyjoys", 
+    "option3" : " Anarchist-targaryens", 
+    "option4" : " Independent- wildlings", 
+    "option5" : " conservatve- lannisters", 
+    "option6" : " moderates-starks", 
+    "answer1": "T",
+    "answer2": "G",
+    "answer3": "M",
+    "answer4": "W",
+    "answer5": "S",
+    "answer6": "L"
 }, {
     "question" : " bbbbbbbaaa?",
-    "option1" : " Abacus", 
-    "option2" : " Aus", 
-    "option3" : " Abus", 
-    "option4" : " Abus", 
-    "option5" : " Abus", 
-    "option6" : " Abus", 
-    "answer": "3"
+    "option1" : " liberals- martells", 
+    "option2" : " libertarians-greyjoys", 
+    "option3" : " Anarchist-targaryens", 
+    "option4" : " Independent- wildlings", 
+    "option5" : " conservatve- lannisters", 
+    "option6" : " moderates-starks", 
+    "answer1": "T",
+    "answer2": "G",
+    "answer3": "M",
+    "answer4": "W",
+    "answer5": "S",
+    "answer6": "L"
 }, {
     "question" : " cccccaaaaaaa?",
-    "option1" : " Abacus", 
-    "option2" : " Aus", 
-    "option3" : " Abus", 
-    "option4" : " Abus", 
-    "option5" : " Abus", 
-    "option6" : " Abus", 
-    "answer": "1"
+    "option1" : " liberals- martells", 
+    "option2" : " libertarians-greyjoys", 
+    "option3" : " Anarchist-targaryens", 
+    "option4" : " Independent- wildlings", 
+    "option5" : " conservatve- lannisters", 
+    "option6" : " moderates-starks", 
+    "answer1": "T",
+    "answer2": "G",
+    "answer3": "M",
+    "answer4": "W",
+    "answer5": "S",
+    "answer6": "L"
 }, {
     "question" : "dddddaaaaaaa?",
-    "option1" : "Abacus", 
-    "option2" : "Aus", 
-    "option3" : "Abus", 
-    "option4" : "Abus", 
-    "option5" : "Abus", 
-    "option6" : "Abus", 
-    "answer": "1"
+    "option1" : " liberals- martells", 
+    "option2" : " libertarians-greyjoys", 
+    "option3" : " Anarchist-targaryens", 
+    "option4" : " Independent- wildlings", 
+    "option5" : " conservatve- lannisters", 
+    "option6" : " moderates-starks", 
+    "answer1": "T",
+    "answer2": "G",
+    "answer3": "M",
+    "answer4": "W",
+    "answer5": "S",
+    "answer6": "L"
 }, {
     "question" : " eeeeaaaaaa?",
-    "option1" : " Abacus", 
-    "option2" : " Aus", 
-    "option3" : " Abus", 
-    "option4" : " Abus", 
-    "option5" : " Abus", 
-    "option6" : " Abus", 
-    "answer": "3"
+    "option1" : " liberals- martells", 
+    "option2" : " libertarians-greyjoys", 
+    "option3" : " Anarchist-targaryens", 
+    "option4" : " Independent- wildlings", 
+    "option5" : " conservatve- lannisters", 
+    "option6" : " moderates-starks", 
+    "answer1": "T",
+    "answer2": "G",
+    "answer3": "M",
+    "answer4": "W",
+    "answer5": "S",
+    "answer6": "L"
 }, {
     "question" : " ffffaaaaaaa?",
-    "option1" : " Abacus", 
-    "option2" : " Aus", 
-    "option3" : " Abus", 
-    "option4" : " Abus", 
-    "option5" : " Abus", 
-    "option6" : " Abus", 
-    "answer": "1"
+    "option1" : " liberals- martells", 
+    "option2" : " libertarians-greyjoys", 
+    "option3" : " Anarchist-targaryens", 
+    "option4" : " Independent- wildlings", 
+    "option5" : " conservatve- lannisters", 
+    "option6" : " moderates-starks", 
+    "answer1": "T",
+    "answer2": "G",
+    "answer3": "M",
+    "answer4": "W",
+    "answer5": "S",
+    "answer6": "L"
 }, {
     "question" : " gggaaaaaaa?",
-    "option1" : " Abacus", 
-    "option2" : " Aus", 
-    "option3" : " Abus", 
-    "option4" : " Abus", 
-    "option5" : " Abus", 
-    "option6" : " Abus", 
-    "answer": "1"
+    "option1" : " liberals- martells", 
+    "option2" : " libertarians-greyjoys", 
+    "option3" : " Anarchist-targaryens", 
+    "option4" : " Independent- wildlings", 
+    "option5" : " conservatve- lannisters", 
+    "option6" : " moderates-starks", 
+    "answer1": "T",
+    "answer2": "G",
+    "answer3": "M",
+    "answer4": "W",
+    "answer5": "S",
+    "answer6": "L"
 }, {
     "question" : " hhhaaaaaaa?",
-    "option1" : " Abacus", 
-    "option2" : " Aus", 
-    "option3" : " Abus", 
-    "option4" : " Abus", 
-    "option5" : " Abus", 
-    "option6" : " Abus", 
-    "answer": "4"
+    "option1" : " liberals- martells", 
+    "option2" : " libertarians-greyjoys", 
+    "option3" : " Anarchist-targaryens", 
+    "option4" : " Independent- wildlings", 
+    "option5" : " conservatve- lannisters", 
+    "option6" : " moderates-starks", 
+    "answer1": "T",
+    "answer2": "G",
+    "answer3": "M",
+    "answer4": "W",
+    "answer5": "S",
+    "answer6": "L"
 }, {
     "question" : " iiiiaaaaaaa?",
-    "option1" : " Abacus", 
-    "option2" : " Aus", 
-    "option3" : " Abus", 
-    "option4" : " Abus", 
-    "option5" : " Abus", 
-    "option6" : " Abus", 
-    "answer": "5"
+    "option1" : " liberals- martells", 
+    "option2" : " libertarians-greyjoys", 
+    "option3" : " Anarchist-targaryens", 
+    "option4" : " Independent- wildlings", 
+    "option5" : " conservatve- lannisters", 
+    "option6" : " moderates-starks", 
+    "answer1": "T",
+    "answer2": "G",
+    "answer3": "M",
+    "answer4": "W",
+    "answer5": "S",
+    "answer6": "L"
 }, {
     "question" : " jjjjaaaaaa?",
-    "option1" : " Abacus", 
-    "option2" : " Aus", 
-    "option3" : " Abus", 
-    "option4" : " Abus", 
-    "option5" : " Abus", 
-    "option6" : " Abus", 
-    "answer": "1"
+    "option1" : " liberals- martells", 
+    "option2" : " libertarians-greyjoys", 
+    "option3" : " Anarchist-targaryens", 
+    "option4" : " Independent- wildlings", 
+    "option5" : " conservatve- lannisters", 
+    "option6" : " moderates-starks", 
+    "answer1": "T",
+    "answer2": "G",
+    "answer3": "M",
+    "answer4": "W",
+    "answer5": "S",
+    "answer6": "L"
 }]
 
 
 
-
-
-// $("#button1").click(function (event) {
-   
-//     window.location.href = "questions.html";
-
-//     });
