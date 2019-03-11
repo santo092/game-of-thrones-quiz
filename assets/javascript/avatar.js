2e9b07faa15b40ec8235dad07ccbc0a421bc4b2a
Alexis Villalpando <alexisvillalpando@Tsmacbookpro.local>
2019-03-11 10:38:44 -0700

    pull

---
+++ assets/javascript/avatar.js
@@ -0,0 +1,12 @@
+// var searchAvatar = function(avatar) {
+//     var queryURL = ("src""https://avatars.dicebear.com/v2/:sprites/:seed.svg";
+//     $.ajax({
+//       url: queryURL,
+//       method: "GET"
+//     }).then(function(response) {
+//      console.log(response);
+//     });
+//   };
+  
+$("img").attr("src", "https://avatars.dicebear.com/v2/avataaars/example.svg")
+
