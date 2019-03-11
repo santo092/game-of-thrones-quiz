e675ebde796a4d202b663ee8f4f681c63e153ada
laura villa <lauravilla@lauras-MacBook-Pro.local>
2019-03-11 14:07:18 -0700

    update

--- assets/javascript/quiz-script.js
+++ assets/javascript/quiz-script.js
@@ -1,4 +1,24 @@
+var config = {
+    apiKey: "AIzaSyCjCOiAuCFRUqmnoV2lZ9pwwczlR7uVh8M",
+    authDomain: "my-project-1551988749749.firebaseapp.com",
+    databaseURL: "https://my-project-1551988749749.firebaseio.com",
+    projectId: "my-project-1551988749749",
+    storageBucket: "my-project-1551988749749.appspot.com",
+    messagingSenderId: "506609611251"
+};
+firebase.initializeApp(config);
 
+var database = firebase.database();
+var userName = $("#userName").val().trim();
+$("#button1").on("click", function () {
+    // event.preventDefault();
+    // window.location.href = "questions.html";
+    userName = $("#userName").val().trim();
+    database.ref().push({
+        userName: userName
+    });
+
+ });
 
 var currentQuestion = 0;
 var score = 0;
@@ -65,6 +85,7 @@ function loadNextQuestion (){
 
         if (score <= 20){
             window.location.href = "targaryen.html";
+
                      
         }
         if ((score <= 40) && (score > 20)){
