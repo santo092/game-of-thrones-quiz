a62b1e7b3887a11eda27f2f8311646377a6e2ea7
laura villa <lauravilla@lauras-MacBook-Pro.local>
2019-03-11 20:29:36 -0700

    update

--- assets/javascript/quiz-script.js
+++ assets/javascript/quiz-script.js
@@ -1,26 +1,4 @@
 
-
-var config = {
-    apiKey: "AIzaSyCjCOiAuCFRUqmnoV2lZ9pwwczlR7uVh8M",
-    authDomain: "my-project-1551988749749.firebaseapp.com",
-    databaseURL: "https://my-project-1551988749749.firebaseio.com",
-    projectId: "my-project-1551988749749",
-    storageBucket: "my-project-1551988749749.appspot.com",
-    messagingSenderId: "506609611251"
-};
-firebase.initializeApp(config);
-
-var database = firebase.database();
-var userName = $("#userName").val().trim();
-$("#button1").on("click", function () {
-    // event.preventDefault();
-    // window.location.href = "questions.html";
-    userName = $("#userName").val().trim();
-    database.ref("/userName").push({
-        userName: userName
-    });
-
- });
 var currentQuestion = 0;
 var score = 0;
 var totQuestions =  questions.length;
