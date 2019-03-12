02c44ce1946723078c6ab654f0b5f4e819876d3f
Shannon King <kingshannon01@gmail.com>
2019-03-12 11:52:03 -0700

    new things

--- assets/javascript/quiz-reults.js
+++ assets/javascript/quiz-reults.js
@@ -1,12 +1,14 @@
 // function goes here to funnel results from
 // quiz into the politicalParty variable
-var politicalParty = "conservative"; 
-var articleKeyWord = politicalParty;
-var queryURL = "" + articleKeyWord + "";
+// var politicalParty = "conservative"; 
+// var articleKeyWord = politicalParty;
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response){
+// for references: https://newsapi.org/docs/endpoints/top-headlines
 
-});
+var queryUrl = "https://newsapi.org/v2/top-headlines?country=us&category=politics&q=conservative&pageSize=8&apiKey=a644d0814d0848ed9c6a8f4400eb2eef"
+var req = new Request(queryUrl);
+
+fetch(req)
+.then(function(response) {
+console.log(response);
+})
