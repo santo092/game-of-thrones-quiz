73a38509eb6fafa3834cd019b0831f6427b00ef0
Shannon King <kingshannon01@gmail.com>
2019-03-13 10:31:47 -0700

    api news function

--- assets/javascript/quiz-results.js
+++ assets/javascript/quiz-results.js
@@ -1,18 +1,53 @@
 
 function getArticlesByCategory(query) {
-    var queryUrl = "https://newsapi.org/v2/everything?q="+query+"&apiKey=a644d0814d0848ed9c6a8f4400eb2eef";
-
-    $.ajax({ url: queryUrl, method: "GET" })
+    var queryUrl = "https://newsapi.org/v2/everything?q=" + query + "&apiKey=a644d0814d0848ed9c6a8f4400eb2eef";
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    })
         .then(function (response) {
             console.log(response);
-            $("#articles_targaryen").append("<p>"+response.articles[0].content+"</p>")
-        })
+            // <a href="#" class="list-group-item list-group-item-action">Arrticle Recommendation</a>
+            for (var i = 0; i < response.articles.length; i++) {
+                var a = $("<a>").text(response.articles[i].title);
+                a.addClass("list-group-item list-group-item-action");
+                a.attr("href", response.articles[i].url);
+                $(".articles").append(a);
+            }
+        });
 }
 
-// if they are a targarian 
-// they are conservative 
-    getArticlesByCategory("conservative");
+// Explanation: function call to run article API - six separate parameters
+// Two possible solutions to the error: 
 
-// if they are a stark
-// they are a liberal
-    getArticlesByCategory("liberal");
+// 1. define all of the house names 
+// 2. replace the house names below with the values that correlate to house names
+
+if (lannisters === true){
+getArticlesByCategory("conservative");
+}
+
+if (martells === true){
+getArticlesByCategory("liberal");
+}
+
+if (stark === true){
+getArticlesByCategory("moderate");
+}
+
+if (wildlings === true){
+getArticlesByCategory("independent");
+}
+
+if (greyjoys === true){
+getArticlesByCategory("libertarian");
+}
+
+if (targaryens === true){
+getArticlesByCategory("anarchy");
+}
+
+
+
+
+
